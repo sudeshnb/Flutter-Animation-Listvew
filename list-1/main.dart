0e7f9bfc202702ed561373d374ecd6456969bbdb
Sudesh Nishshanka Bandara <33403844+sudeshnb@users.noreply.github.com>
2023-03-11 19:10:42 +0530

    Create main.dart

---
+++ list-1/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'homescreen.dart';
+import 'listscreen.dart';
+import 'mainscreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
